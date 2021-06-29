38651c897bedf2d2cb55d0e6436c311208ca5ffa
kaiyu li <krisfk@gmail.com>
2021-06-30 01:53:59 +0800

    .

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -5,7 +5,12 @@
     <a href="#" class="nav-news-btn">
     </a>
 
-    <img class="logo" src="{{url('/images/djs-logo.png')}}"><span>{{trans("lang.shop_name")}}
-    </span>
+
+
+    <div class="d-flex align-center">
+    <img class="logo" src="{{url('/images/djs-logo.png')}}">
+    
+    <span>{{trans("lang.shop_name")}}</span>
+    </div>
 
 </div>
